0b49341df5650a8499c64536551407f736869fe1
usmailaabdoul <ismaelabdul77@gmail.com>
2021-05-10 07:52:47 +0100

    Fixing issues

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 require('dotenv').config();
 const createServer = require('./src/server');
 const setUpMongoose = require('./config/mongoose');
-const appConfig = require('./app.config');
 
+let url = {mongoUrl: process.env.MONGODB_URL || 'mongodb://localhost/carnaProject'}
 async function init() {
-  await setUpMongoose(appConfig.mongoUrl);
+  await setUpMongoose(url.mongoUrl);
 
   return createServer();
 }
 
 init().then(server => {
-  server.listen(appConfig.PORT, () => {
-    console.log(`app is running on port ${appConfig.PORT}`);
+  server.listen(process.env.PORT, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
   })
 })
-
-if (process.env.NODE_ENV === 'production') {
-  // Catch any uncaught exceptions in this application
-  process.on('uncaughtException', (err) => {
-    console.log(`There was an uncaught exception: ${err}`);
-  });
-
-  // Catch any unhandled rejections in this application
-  process.on('unhandledRejection', (err) => {
-    console.log(`There was an unhandled rejection: ${err}`);
-  });
-}
